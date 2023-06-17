25c29c77adc7f0389294a6ca2fa628521f436bad
SrDem <shadowofthedeadwolves@gmail.com>
2023-06-18 02:26:51 +0530

    hehe

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,14 @@
+import 'package:bijan_i/pages/home_page.dart';
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 
 import '../services/firebase_auth_helper.dart';
+=======
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../providers/fakeuser.dart';
+>>>>>>> abc7cfe (hehe)
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -8,11 +16,20 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: Stack(
         children: [
-          const Align(
-            alignment: Alignment.center,
-            child: SafeArea(child: Text('e')),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/wavy_background.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
           ),
           Positioned(
             top: 0,
@@ -23,18 +40,19 @@ class LoginPage extends StatelessWidget {
                 child: Text(
                   "Login",
                   style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                        color: Colors.black,
+                        color: Theme.of(context).colorScheme.primary,
                         fontWeight: FontWeight.w200,
+                        fontSize: 100,
                       ),
                 ),
               ),
             ),
           ),
-          Positioned(
-            bottom: 16.0,
-            right: 16.0,
+          Align(
+            alignment: Alignment.center,
             child: Column(
               children: [
+<<<<<<< HEAD
                 InkWell(
                   onTap: () async {
                     await FirebaseAuthHelper.signInWithGoogle();
@@ -46,15 +64,51 @@ class LoginPage extends StatelessWidget {
                       padding: EdgeInsets.all(6),
                       child: Wrap(
                         crossAxisAlignment: WrapCrossAlignment.center,
+=======
+                Spacer(),
+                //need to add Sized boxes for this more buttons.
+                Consumer(builder: (context, ref, child) {
+                  return OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        width: 2,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      ref.read(userProvider.notifier).state = 'not default';
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => HomePage(),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+>>>>>>> abc7cfe (hehe)
                         children: [
-                          Icon(Icons.android),
-                          SizedBox(width: 12),
-                          Text('Sign in with Google'),
+                          FaIcon(FontAwesomeIcons.google),
+                          SizedBox(width: 15),
+                          Text(
+                            "Login with Google",
+                            style:
+                                Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                      fontSize: 25,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                          ),
                         ],
                       ),
                     ),
-                  ),
-                )
+                  );
+                }),
+                Spacer(),
               ],
             ),
           )
@@ -63,3 +117,26 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+
+
+//void main() {
+//  runApp(
+//    ProviderScope(
+//      child: MaterialApp(
+//        debugShowCheckedModeBanner: false,
+//        home: StartPage(),
+//        // routes: ,
+//        theme: ThemeData.from(
+//          colorScheme: ColorScheme.fromSeed(
+//            seedColor: Color.fromRGBO(8, 103, 136, 1),
+//            background: Color.fromRGBO(255, 241, 208, 1),
+//            secondary: Color.fromRGBO(240, 200, 8, 1),
+//            primary: Color.fromRGBO(8, 103, 136, 1),
+//            tertiary: Color.fromRGBO(6, 174, 213, 1),
+//            error: Color.fromRGBO(221, 28, 26, 1),
+//          ),
+//        ),
+//      ),
+//    ),
+//  );
+//}
