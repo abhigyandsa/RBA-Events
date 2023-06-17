9e9ded7259840ff3ce6982167423da282ad9d17f
SrDem <shadowofthedeadwolves@gmail.com>
2023-06-18 02:56:00 +0530

    heheaaraar

---
+++ lib/widgets/qr_scanner.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/qr_data_provider.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class Scanner extends ConsumerStatefulWidget {
+  const Scanner({super.key});
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ScannerState();
+}
+
+class _ScannerState extends ConsumerState<Scanner> {
+  late final MobileScannerController cameraController;
+
+  @override
+  void initState() {
+    super.initState();
+    cameraController = MobileScannerController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cameraController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mobile Scanner'),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
+              },
+            ),
+            onPressed: () => cameraController.toggleTorch(),
+          ),
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.cameraFacingState,
+              builder: (context, state, child) {
+                switch (state as CameraFacing) {
+                  case CameraFacing.front:
+                    return const Icon(Icons.camera_front);
+                  case CameraFacing.back:
+                    return const Icon(Icons.camera_rear);
+                }
+              },
+            ),
+            onPressed: () => cameraController.switchCamera(),
+          ),
+        ],
+      ),
+      body: MobileScanner(
+        controller: cameraController,
+        fit: BoxFit.contain,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          ref.read(qrdataProvider.notifier).state =
+              barcodes[barcodes.length - 1].rawValue;
+          Navigator.popUntil(context, (Route<dynamic> route) => route.isFirst);
+        },
+      ),
+    );
+  }
+}
