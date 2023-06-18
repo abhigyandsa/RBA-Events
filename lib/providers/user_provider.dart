3745eb655bc5a21164e0583b3e4dbfbd0c953bab
SrDem <shadowofthedeadwolves@gmail.com>
2023-06-18 15:41:45 +0530

    pretty login

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rba/services/user_information.dart';
+import 'package:rba/widgets/user_information.dart';
 
 final userProvider =
     NotifierProvider<UserNotifier, UserInformation?>(UserNotifier.new);
@@ -11,30 +11,21 @@ class UserNotifier extends Notifier<UserInformation?> {
     return null;
   }
 
-  void initUsrData(String email) {
-    state = UserInformation(email: email);
-  }
-
-  void getnsetData() async {
-    final email = state!.email;
+  void login(String email) async {
     final userInformation = await FirebaseFirestore.instance
         .collection('users')
         .where('email', isEqualTo: email)
         .get();
-    // final String? name = userInformation.docs.first.data()['name'];
-    // final String? phone = userInformation.docs.first.data()['phone'];
-    // final int? adminlevel = userInformation.docs.first.data()['adminlevel'];
+    final String? name = userInformation.docs.first.data()['name'];
+    final String? phone = userInformation.docs.first.data()['phone'];
+    final int? adminlevel = userInformation.docs.first.data()['adminlevel'];
 
     state = UserInformation(
       email: email,
-      name: 'name',
-      adminlevel: 1,
-      phone: 'phone',
+      name: name,
+      adminlevel: adminlevel,
+      phone: phone,
     );
-  }
-
-  void setNull() {
-    state = null;
   }
 
   void userInformationUpdate() {
