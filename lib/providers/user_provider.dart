1375603a9beeacfde835121202c5f03969766fe3
abhigyandsa <abhigyandsa@gmail.com>
2023-06-19 03:18:02 +0530

    some stuff

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -19,16 +19,49 @@ class UserNotifier extends Notifier<UserInformation?> {
     final email = state!.email;
     final db = FirebaseFirestore.instance;
     await db.collection("users").get().then((event) {
+      var docRef = null;
       for (var doc in event.docs) {
         if (doc.data()['email'] == email) {
+          docRef = doc.reference;
+          break;
+        }
+      }
+      if (docRef == null) {
+        state = UserInformation(
+          email: email,
+          adminlevel: 0,
+        );
+        UserInformation(
+          email: email,
+          adminlevel: 0,
+        ).postUserInformation();
+      } else {
+        docRef.get().then((doc) {
           state = UserInformation(
             email: email,
             name: doc.data()['name'],
             adminlevel: doc.data()['adminlevel'],
             phone: doc.data()['phone'],
           );
-        }
+        });
+        docRef.snapshots().listen(
+          (event) {
+            state = UserInformation(
+              email: email,
+              name: event.data()['name'],
+              adminlevel: event.data()['adminlevel'],
+              phone: event.data()['phone'],
+            );
+          },
+          onError: (error) => print("Listen failed: $error"),
+        );
       }
+      // state = UserInformation(
+      //       email: email,
+      //       name: doc.data()['name'],
+      //       adminlevel: doc.data()['adminlevel'],
+      //       phone: doc.data()['phone'],
+      //     );
     });
 
     // final userInformation = await FirebaseFirestore.instance
